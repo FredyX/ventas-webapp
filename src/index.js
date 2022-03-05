41b616bdda102b626fcc878b25ba6abc654cfd0b
Alejandra <alejandraramirez9713@gmail.com>
2022-03-04 20:16:07 -0600

    creando los componentes paara los mensajes de exito y error de la pantalla de registro

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/main.css';
+import Successfulmessage from './components/successfulmessage';
+import Errormessage from './components/errormessage1';
+import Errormessage2 from './components/errormessage2';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Successfulmessage />
+    <Errormessage />
+    <Errormessage2 />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
