b53abf21eb3a262be5ca5a74c77e58b028d0465f
darkbehm <bbeehhmm@protonmail.com>
2022-03-08 10:22:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route} from "react-router-dom";
 import {Home} from "./Components/Pages/Home/Home";
 import {Registro} from "./Components/Registro/Registro";
 
+//semana 1
 
 function App() {
 
