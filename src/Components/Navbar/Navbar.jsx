4668c0f9a201898aa2b4928838a2e324cde1ab06
darkbehm <bbeehhmm@protonmail.com>
2022-03-09 10:32:06 -0600

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,50 @@
 import styles from "./Navbar.module.scss";
-import {NavLink, Link} from "react-router-dom";
-import {BsArrowRight} from "react-icons/bs";
-import {FaBars, FaTimes} from "react-icons/fa";
+import { NavLink, Link } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
 
-const Navbar = ({BurgerColour}) => {
+const Navbar = ({ BurgerColour }) => {
 
-const MenuLink=({url, path})=>{
-    return <li className={styles.NavLink}>
-        <NavLink to = {`${url}`} className={({isActive})=>(isActive ? styles.active : undefined)}>
-            {`${path}`}
-        </NavLink>
+    const MenuLink = ({ url, path }) => {
+        return <li className={styles.NavLink}>
+            <NavLink to={`${url}`} className={({ isActive }) => (isActive ? styles.active : undefined)}>
+                {`${path}`}
+            </NavLink>
         </li>
-}
+    }
 
-const [isNavOpen, setisNavOpen] = useState(false);
-
-
-  return (
-    <div className={styles.navbar_container}>
-        <nav>
-            {/* LOGO */}
-            <div className={styles.brand_logo}>
-                <Link to="/">Real</Link>
-            </div>
-
-            {/* NAV-BURGER */}
-            <div className={styles.mobile_menu}
-            style={{color: BurgerColour}}
-            onClick={()=>setisNavOpen(!isNavOpen)}
-            >
-                <FaBars/>
-            </div>
-
-            {/* NAV */}
-            <ul className={`${isNavOpen ? styles.ul_active : undefined} ${styles.navbar_ul}`}>
-
-                <div className={styles.mobile_close} OnClick={()=> setisNavOpen(!isNavOpen)}>
-                    <FaTimes/>
+    const [isNavOpen, setisNavOpen] = useState(false);
+    return (
+        <div className={styles.navbar_container}>
+            <nav>
+                {/* LOGO */}
+                <div className={styles.brand_logo}>
+                    <Link to="/">Real</Link>
                 </div>
 
-            {/* LI - MENULINK*/}
-            <MenuLink url="" path="Home" />
-            <MenuLink url="" path="Home" />
-            </ul>
-        </nav>
-    </div>
-  )
+                {/* NAV-BURGER */}
+                <div className={styles.mobile_menu}
+                    style={{ color: BurgerColour }}
+                    onClick={() => setisNavOpen(!isNavOpen)}
+                >
+                    <FaBars />
+                </div>
+
+                {/* NAV */}
+                <ul className={`${isNavOpen ? styles.ul_active : undefined} ${styles.navbar_ul}`}>
+
+                    <div className={styles.mobile_close} OnClick={() => setisNavOpen(!isNavOpen)}>
+                        <FaTimes />
+                    </div>
+
+                    {/* LI - MENULINK*/}
+                    <MenuLink url="" path="Home" />
+                    <MenuLink url="" path="Home" />
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar;
