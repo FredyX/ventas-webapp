91ef7b32a0607640215ec11966ac473d1164d30c
darkbehm <bbeehhmm@protonmail.com>
2022-03-09 20:08:01 -0600

    footer fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.scss";
 import "./Components/Registro/Registro.scss";
+import "./Components/Footer/footer.scss";
 import {Routes, Route} from "react-router-dom";
 import {Home} from "./Components/Pages/Home/Home";
 import {Registro} from "./Components/Registro/Registro";
 import {InicioSesion} from "./Components/InicioSesion/InicioSesion";
 import {TerminosDeServicio} from "./Components/Pages/ToS/TerminosDeServicio";
-
+import {Footer} from "./Components/Footer/Footer";
 //semana 1
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Route path="/" element= {<Home />} /> 
       <Route path="/tos/" element= {<TerminosDeServicio />} />
       </Routes>
+      <Footer />
     </div>
   );
 } 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,38 +1,75 @@
 import React from "react";
 import userDataService from "../../services/users.service";
 import { Link } from "react-router-dom";
-import styles from "\footer.scss";
+import styles from ".//footer.scss";
+import styled from 'styled-components';
 
-const Footer = () => {
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
+`;
 
+const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-gap: 20px;
+  box-shadow: 20px 20px 20px 0 rgba(0, 0, 0, 0.19);
+  outline: solid rgb(18, 183, 0);
+  background-color: #1b1b1b;
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+`;
+
+const FooterLink = styled.a`
+  color: #ffffff;
+  margin-bottom: 15px;
+  font-size: 18px;
+  text-decoration: none;
+  &:hover {
+      color: #ff9c00;
+      transition: 200ms ease-in;
+  }
+`;
+const Heading = styled.p`
+  padding: 30px 0px;
+  font-size: 24px;
+  color: #12b700;
+  margin-bottom: 5px;
+  font-weight: bold;
+`;
+export class Footer extends React.Component {
+  render(){
     return(
-        <footer classname="foot">
-        <div classname="conteiner">
+    <footer classname="foot">
+      <div classname="conteiner">
         <Row classname="ro">
-          <Column classname = "col">
+          <Column/>
+          <Column classname="col">
             <Heading>Acerca de nosotros</Heading>
             <FooterLink href="#">Seguridad</FooterLink>
             <FooterLink href="#">Privacidad</FooterLink>
             <FooterLink href="#">Terminos y condiciones</FooterLink>
+            <FooterLink/>
           </Column>
 
-          <Column classname = "col">
-            <Heading classname="he">Contactos y categorias</Heading>
-            <FooterLink href="#">Categorias</FooterLink>
-            <FooterLink href="#">Departamentos</FooterLink>
+          <Column classname="col">
+            <Heading classname="he">Contacto</Heading>
             <FooterLink href="#">Contactenos</FooterLink>
+            <FooterLink href="#">Preguntas frecuentes</FooterLink>
           </Column>
 
-          <Column classname = "col">
+          <Column classname="col">
             <Heading>Mi cuenta</Heading>
             <FooterLink href="#">Iniciar Sesion</FooterLink>
             <FooterLink href="#">Registro</FooterLink>
           </Column>
-         
-        </Row>
-        </div>
-        </footer>
-    );
-export default Footer;
 
+        </Row>
+      </div>
+    </footer>
+);
+}
 }
