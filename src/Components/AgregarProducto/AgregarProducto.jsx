1aa1f03eb713907e354a4adc311f903a1946bdf7
aleramirez9713 <62529782+aleramirez9713@users.noreply.github.com>
2022-03-19 21:05:39 -0600

    Cambio de clase a función - Pantalla Agregar Producto

--- src/Components/AgregarProducto/AgregarProducto.jsx
+++ src/Components/AgregarProducto/AgregarProducto.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import userDataService from "../../services/users.service";
 import styled from 'styled-components';
 import styles from "./AgregarProducto.scss";
 import CheckboxList from './checkboxlist';
 import PinnedSubheaderList from './checkboxlist';
 import DragAreaPrincipal from './imagenprevious';
 
+
+export const AgregarProducto = (props) => {
 
 const Column = styled.div`
 display: flex;
@@ -26,100 +27,6 @@ position: center;
 bottom: 0;
 width: 100%;
 `;
-
-
-
-export class AgregarProducto extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          title: "",
-          price: "",
-          category: "",
-          state: "",
-          description: ""   
-  
-        };    
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleBlur = this.handleBlur.bind(this);
-        this.isObjectEmpty = this.isObjectEmpty.bind(this);
-      }
-  
-  
-      
-      handleInputChange(event) {
-          const target = event.target;
-          const value = target.value;
-          const name = target.name;
-  
-          this.setState({
-            [name]: value
-          });
-        }
-        isObjectEmpty(obj) {
-          for (let  prop in obj){
-            if(obj.hasOwnProperty(prop))return false;    
-          }
-          return true;
-        }
-  
-        handleSubmit() {
-          if(this.isObjectEmpty(this.state.errors)){
-          
-            let data = {
-              title: this.state.first_name,
-              price: this.state.last_name,
-              category: this.state.user_email,
-              state: this.state.user_password,
-              description: this.state.department_id,
-            }
- 
-            userDataService.register(data)
-              .then(response => {
-                this.setState({
-                  title: "",
-                  price: "",
-                  category: "",
-                  state: "",
-                  description: ""
-                });
-                console.log(response.data);
-              })
-              .catch(e => {
-                console.log(e);
-              });
-          }else{
-            alert('Debe llenar todos los campos');
-          }
-        }
-        
-
-        handleBlur(e) {
-          //this.handleInputChange(e);
-          const errores = this.validateForm();
-          const name  = 'errors';
-          this.setState({
-            [name]:errores
-          });
-          console.log(errores);
-        }
-        validateForm(){    
-          let errors = {};
-          if (!this.state.title.trim()){
-            errors.title = 'El campo del título es obligatorio';
-          }
-          if(!this.state.price.trim()){
-            errors.price = 'El campo precio es obligatorio';
-          }
-          if(!this.state.description.trim()){
-              errors.description = 'El campo descripción es obligatorio';
-          }
-          return errors;
-        }
-
-
-    render() {
         return (
     
             <div classname="conteiner">
@@ -138,27 +45,21 @@ export class AgregarProducto extends React.Component {
                       <div className="form-group">
                         <input
                           type="text"
-                          value={this.state.title}
                           name="title"
-                          onChange={(e) => this.handleInputChange(e)}
                           placeholder="Título" 
-                          onBlur={(e) => this.handleBlur(e)}
                           />
                       </div>
 
                       <div className="form-group">
                         <input
                         type="text" 
-                        value={this.state.price} 
                         name="price" 
-                        onChange={(e) => this.handleInputChange(e)} 
                         placeholder="Precio"
-                        onBlur={(e) => this.handleBlur(e)}
                         />
                       </div>
 
                       <div className="caja">
-                        <select name="state" value={this.state.state} onChange={(e) => this.handleInputChange(e)}>
+                        <select name="state"  >
                           <option >Estado</option>
                           <option value={1}>Nuevo</option>
                           <option value={2}>Usado</option>
@@ -168,22 +69,16 @@ export class AgregarProducto extends React.Component {
                       <div className="form-group2">
                         <input 
                           type="text" 
-                          value={this.state.description} 
                           name="description" 
-                          onChange={(e) => this.handleInputChange(e)} 
                           placeholder="Descripción"
-                          onBlur={(e) => this.handleBlur(e)}
                         />
                       </div>
 
-
-                      
                       <span>Seleccione una o varias categorías</span>
                       <PinnedSubheaderList/>                 
-
                       </div>
                           <div className="footer">
-                          <button type="button" className="btn" onClick={this.handleSubmit}>
+                          <button type="button" className="btn" >
                               Agregar producto
                           </button>
                           <Link to ={"/"}>
@@ -203,20 +98,15 @@ export class AgregarProducto extends React.Component {
                   <div className="imageproduct-form" >
                   <div className="header2">Imagenes del producto</div>
                     <div className="form">
-                      
-                      
+
                       <DragAreaPrincipal/>
-                                   
-                     
+
                       </div>
                     </div>
                   </div>
                 </Column>
               </Row>
             </div>
-
         );
-        
-    }
-  }
-
+ 
+}
