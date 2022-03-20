429546c4efaa62a4ee37e235d3f0352a06b5d365
aleramirez9713 <62529782+aleramirez9713@users.noreply.github.com>
2022-03-20 11:24:21 -0600

    useFormProdcut y validateFormProdcut

---
+++ src/helpers/validateFormProductos.js
@@ -0,0 +1,30 @@
+import { regularExp } from './regularExp';
+
+const validateFormProducts = (stateForm, check = null) => {
+
+  let errors = {};
+  
+  if (!stateForm.title) {
+    errors.title = 'El campo del título es obligatorio';
+  }
+  if (!stateForm.price) {
+    errors.price = 'El campo precio es obligatorio';
+  }
+  if (!stateForm.description) {
+    errors.description = 'El campo descripción es obligatorio';
+  }
+  if (!stateForm.price) {
+    errors.last_name = 'El campo apellido es obligatorio';
+  }
+
+  if (!(check === null)) { 
+    if (!check) {
+      errors.terms = 'Necesita seleccionar al menos una categoría';
+    }
+  }
+
+  return errors;
+}
+
+
+export { validateFormProducts };
