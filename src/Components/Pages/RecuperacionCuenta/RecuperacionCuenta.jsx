b0d46df6d862d8f20e6fbf23fdde9c322b6061d6
FredyX <feamador@unah.hn>
2022-03-30 14:31:05 -0600

    fusion

--- src/Components/Pages/RecuperacionCuenta/RecuperacionCuenta.jsx
+++ src/Components/Pages/RecuperacionCuenta/RecuperacionCuenta.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../Navbar/Navbar.module.scss";
-
+import {useState} from 'react';
 
 export const RecuperacionCuenta = (props) => {
+    const [user_email, setUserEmail] = useState({});
+    
+    const handleChange = ({target})=>{
+      setUserEmail({
+        [target.name] : target.value
+      });
+    }
+    const enviar = ({target}) => {
+      let formData = new FormData();
+      formData.append('user_email', user_email.user_email);
 
+      fetch('http://192.168.0.6:3001/api/forgot_password',{
+        headers:{
+          "Content-Type": "application/x-www-form-urlencoded",
+          //"Content-Type": "multipart/form-data"
+        },
+        method: "post",
+
+      })
+        .then( res => console.log('Hola'))
+        .catch(res => console.log(res));
+    }
     const style = {
       fontWeight: "bold",
       color: "#dc3545"
@@ -47,11 +68,12 @@ export const RecuperacionCuenta = (props) => {
                   type="text"
                   name="user_email"
                   placeholder="Correo electrónico"
+                  onChange={handleChange}
                 />
               </div>
           </div>
           <div className="footer">
-            <button type="button" className="btn" >
+            <button type="button" className="btn" onClick={enviar} >
               Enviar Correo
             </button>
           </div>
